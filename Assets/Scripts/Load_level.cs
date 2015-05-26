095ed19acffee610488ceeb8615264702aab8abc
TontonTremblay <tonton.tremblay@gmail.com>
2015-05-26 10:41:55 -0400

    woprking

---
+++ Assets/Scripts/Load_level.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Load_level : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame()
+	{
+		Application.LoadLevel("dev1");
+	}
+}
---
+++ Assets/Scripts/Recorder.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Recorder : MonoBehaviour 
+{
+	public List<Vector3> posBeen = new List<Vector3>(); 
+	public List<Vector3> lookAt = new List<Vector3>(); 
+	// Use this for initialization
+	public float timeRecording = 0.1f;
+	private float timeStart; 
+
+	void Start () 
+	{
+		posBeen.Clear();
+		lookAt.Clear();
+		timeStart = 0; 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timeStart+=Time.deltaTime;
+		if (timeStart> timeRecording)
+		{
+			posBeen.Add(gameObject.transform.position);
+			lookAt.Add(gameObject.transform.forward); 
+			timeStart = 0; 
+		}
+	}
+}
