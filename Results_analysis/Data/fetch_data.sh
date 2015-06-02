530d7625ebe9279ac5a6fdc69d77ba11ee76eb9d
TontonTremblay <tonton.tremblay@gmail.com>
2015-06-02 10:59:38 -0400

    rock and roll, heatmap for each level produced.

--- Results_analysis/Data/fetch_data.sh
+++ Results_analysis/Data/fetch_data.sh
@@ -1,1 +1,3 @@
-wget -r --no-parent http://www.cs.mcgill.ca/~jtremb59/FPS_Study/upload/*.txt
+wget -r --no-parent -e robots=off --reject="index.html*"  http://www.cs.mcgill.ca/~jtremb59/FPS_Study/upload/i
+mv www.cs.mcgill.ca/~jtremb59/FPS_Study/upload/* .
+rm -rf www.cs.mcgill.ca/
